06788393aa4adec1dac5bb571c6ab27ffb286d9f
Ozarbi Afashokov <oafashokov@yandex-team.ru>
2020-09-05 22:16:00 +0300

    Make fixes on comments

--- src/main/java/com/handtruth/lessons/lesson1/task1/Simple.java
+++ src/main/java/com/handtruth/lessons/lesson1/task1/Simple.java
@@ -8,8 +8,7 @@ class Simple {
      * Вычислить дискриминант квадратного уравнения
      */
     double discriminant(double a, double b, double c) {
-        //TODO your code
-        return b*b-4*a*c;
+        return b * b - 4 * a * c;
     }
 
     /**
@@ -18,7 +17,7 @@ class Simple {
      * Поиск одного из корней квадратного уравнения
      */
     double sqRoot(double a, double b, double c) {
-        return (discriminant(a,b,c) - b)/(2*a);
+        return (discriminant(a, b, c) - b) / (2 * a);
     }
 
     /**
@@ -28,8 +27,7 @@ class Simple {
      * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
      */
     double seconds(int hours, int minutes, int seconds) {
-        //TODO your code
-        return hours*3600+minutes*60+seconds;
+        return hours * 3600 + minutes * 60 + seconds;
     }
 
     /**
@@ -38,7 +36,6 @@ class Simple {
      * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
      */
     int numberRevert(int number) {
-        //TODO your code
-        return number%10*100+number%100/10*10+number/100;
+        return number % 10 * 100 + number % 100 / 10 * 10 + number / 100;
     }
 }
