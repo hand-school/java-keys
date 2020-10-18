e4951c4f3076f6c7cc4f4f4d739c0c5cb5cb929b
lexcorp <lr.romanov1999@mail.ru>
2020-10-18 12:30:03 +0300

    Задания с массивами к уроку 4 задание 1

--- src/main/java/com/handtruth/lessons/lesson4/task1/ArrayTask.java
+++ src/main/java/com/handtruth/lessons/lesson4/task1/ArrayTask.java
@@ -1,4 +1,36 @@
 package com.handtruth.lessons.lesson4.task1;
 
 public class ArrayTask {
+
+    //  Easy
+//  На вход передается массив, состоящий из фамилий
+//  А также буква
+//  Вывести количетсво фамилий, начинающихся на эту букву
+//  ИГНОРИРОВТЬ case
+    public static int filterPeoples(String[] lastNames, char filter) {
+        return -1;
+    }
+
+    //  Easy
+//  На вход передается массив из чисел
+//  Заменить в исходном массиве все отрицательные числа на 0
+//  Вернуть полученный массив
+    public static int[] changeNumbers(int[] numbers) {
+        return numbers;
+    }
+
+    //  Easy
+//  Необходио развернуть массив
+    public static int[] revertArray(int[] array) {
+        return array;
+    }
+
+
+    //  Medium
+//  Необходимо вернуть все различные элементы массивов
+//  Примечание:  значения в массивах отсортированы по возрастанию
+    public static int[] uncommonPartOfArr(int[] arr1, int[] arr2) {
+        return null;
+    }
+
 }
--- src/test/java/com/handtruth/lessons/lesson4/task1/ArrayTaskTest.java
+++ src/test/java/com/handtruth/lessons/lesson4/task1/ArrayTaskTest.java
@@ -1,4 +1,56 @@
 package com.handtruth.lessons.lesson4.task1;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.handtruth.lessons.lesson4.task1.ArrayTask.changeNumbers;
+import static com.handtruth.lessons.lesson4.task1.ArrayTask.filterPeoples;
+import static com.handtruth.lessons.lesson4.task1.ArrayTask.uncommonPartOfArr;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArrayTaskTest {
+
+    @Test
+    @Tag("Easy")
+    void filterPeoplesTest() {
+        String[] arr = new String[]{"Петров", "Иванов", "сидоров"};
+
+        assertEquals(1, filterPeoples(arr, 'C'));
+    }
+
+    @Test
+    @Tag("Easy")
+    void changeNumbersTest() {
+        int[] arr = new int[]{1, 2, 3, 0, -2, -3};
+        int[] expected = new int[]{1, 2, 3, 0, 0, 0};
+
+        assertArrayEquals(expected, changeNumbers(arr));
+        assertArrayEquals(new int[0], new int[0]);
+    }
+
+    @Test
+    @Tag("Easy")
+    void revertArrayTest() {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        int[] expected = new int[]{6, 5, 4, 3, 2, 1};
+
+        assertArrayEquals(expected, changeNumbers(arr));
+        assertArrayEquals(new int[0], new int[0]);
+    }
+
+
+    @Test
+    @Tag("Medium")
+    void uncommonPartOfArrTest() {
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 11, 123, 324, 456};
+        int[] arr2 = new int[]{-11, -5, 0, 1, 2, 3, 4, 5, 23, 331, 456, 1024};
+        int[] actual = uncommonPartOfArr(arr1, arr2);
+        int[] expected = new int[]{-11, -5, 0, 11, 123, 324, 331, 1024, 23};
+
+        assertArrayEquals(expected, actual);
+        assertArrayEquals(new int[0], new int[0]);
+    }
+
+
 }
